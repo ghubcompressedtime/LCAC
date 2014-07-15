efcab90bcd280ca27f723921e24aed3eaad60f16
unknown <github@compressedtime.com>
2014-07-15 01:54:33 -0400

    OK

--- LCAC.user.js
+++ LCAC.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           LCAC
 // @namespace      compressedtime.com
-// @version        3.196
+// @version        3.197
 // @run-at         document-end
 // @grant          GM_getValue
 // @grant          GM_setValue
